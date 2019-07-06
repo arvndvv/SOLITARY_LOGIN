0066b1ff88cfe78f54d80935800dbd661635dced
Aravind V V <aravind16498@gmail.com>
2019-07-07 00:24:15 +0530

    Add files via upload

---
+++ java/myscripts.js
@@ -0,0 +1,20 @@
+function reg() {
+       
+     
+        var signup = document.querySelector(".box-signup");
+        var login = document.querySelector(".box-login")
+        login.style.left = "-500vw";
+  
+      signup.style.left = "50%";
+   
+    }
+function log() {
+       
+     
+        var signup = document.querySelector(".box-signup");
+        var login = document.querySelector(".box-login")
+        login.style.left = "50%";
+  
+      signup.style.left = "-500vw";
+   
+    }
